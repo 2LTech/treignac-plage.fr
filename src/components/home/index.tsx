a016defedeef7c8735d43eb359b8207c3df0244d
Simon Garnotel <simon.garnotel@gmail.com>
2026-09-18 10:15:07 +0200

    update & update menu

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -36,18 +36,18 @@ const menus = [
     file: '/menu/0914_menu_semaine.jpg',
     label: 'Menus de la Semaine'
   },
-  {
-    key: 'mecredi',
-    logo: 'hat.svg',
-    file: '/menu/0916_menu_mercredi.jpg',
-    label: 'Menu du Mercredi Soir'
-  },
   // {
-  //   key: 'samedi',
+  //   key: 'mecredi',
   //   logo: 'hat.svg',
-  //   file: '/menu/0912_menu_samedi.jpg',
-  //   label: 'Menus du Samedi Midi'
+  //   file: '/menu/0916_menu_mercredi.jpg',
+  //   label: 'Menu du Mercredi Soir'
   // },
+  {
+    key: 'samedi',
+    logo: 'hat.svg',
+    file: '/menu/0919_menu_samedi.jpg',
+    label: 'Menus du Samedi Midi'
+  },
   {
     key: 'vin',
     logo: 'wine.svg',
